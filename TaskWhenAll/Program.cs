adf37a880649c72a76d81393c5b458721d66512b
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-08-13 14:43:24 -0300

    refactor: Adicionado tipo e variável para receber resultado da ação no 'Service'

--- TaskWhenAll/Program.cs
+++ TaskWhenAll/Program.cs
@@ -4,7 +4,7 @@ Service service = new();
 
 Console.WriteLine(DateTime.UtcNow);
 
-await service.ExecuteAsync();
-await service.ExecuteAsync();
+Result result1 = await service.ExecuteAsync();
+Result result2 = await service.ExecuteAsync();
 
 Console.WriteLine(DateTime.UtcNow);
